d14eeb3843947824155877acd51d60bb07f7870c
LainezDev <38702185+LainezDev@users.noreply.github.com>
2024-09-22 17:17:28 -0500

    correccion DATE_FORMAT FormMaster.js
    Corrección

--- FormMaster.js
+++ FormMaster.js
@@ -142,7 +142,7 @@ function onError(error) {
 };
 
 function loadForm(configJson, GetFormData) {
-    DATE_FORMAT = config.dateformat;
+    DATE_FORMAT = configJson.dateformat;
     const formContainer = document.getElementById('form-container');
     const formGroup = document.createElement('form');
     const formName = 'form' + configJson.tablename;
